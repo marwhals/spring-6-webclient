3a442d7459fc0ae735bfc4ef21bfa6bffaac8a72
marwhals <marjan.mubarok2012@gmail.com>
2025-04-26 23:52:17 +0100

    Create a web client

---
+++ src/main/java/com/spring_6_webclient/spring_6_webclient/client/BeerClient.java
@@ -0,0 +1,28 @@
+package com.spring_6_webclient.spring_6_webclient.client;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.spring_6_webclient.spring_6_webclient.model.BeerDTO;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+import java.util.Map;
+
+public interface BeerClient {
+
+    Flux<String> listBeer();
+
+    Flux<Map> listBeerMap();
+
+    Flux<JsonNode> listBeersJsonNode();
+
+    Flux<BeerDTO> listBeerDtos();
+
+    Mono<BeerDTO> getBeerById(String id);
+
+    Flux<BeerDTO> getBeerByBeerStyle(String beerStyle);
+
+    Mono<BeerDTO> createBeer(BeerDTO beerDTO);
+
+    Mono<BeerDTO> updateBeer(BeerDTO beerDTO);
+
+}
